ea8cf3d8344d62ffca2e0edac6e92b4f9366eef6
DESKTOP-4LA1ITV\Student <ccs@cspc>
2024-08-16 12:51:53 +0800

    PS1

---
+++ Parco_Rigel/PS1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print("Hello World");
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World",
+          style: TextStyle(fontStyle: FontStyle.italic)
+          )
+        )
+      )
+    )
+  );
+}
