e9f77e13474d7f967c5cae685b7ff9aeaa3360e8
Gian Carlo Bata <nebulareclipse17@gmail.com>
2024-09-09 19:26:37 +0800

    Refactored the code

--- Bata_GianCarlo/PS1/app.py
+++ Bata_GianCarlo/PS1/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request # type: ignore
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    name = None
+    
+    if request.method == "POST":
+        name = request.form.get("name")
 
-@app.route("/submit", methods=["POST"])
-def submit():
-    name = request.form["name"]
-    return render_template("greeting.html", name=name)
+    return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
